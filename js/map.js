68360e349be7f12389c96b8a344de577890d0627
Pedro Knoll <p.giacometo@gmail.com>
2018-07-27 21:25:49 -0300

    Add menu button with animations

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 function initMap(){
 
   var mapCenterPoint = {
-    lat: -23.581346,
-    lng: -46.684708
+    lat: -23.582944,
+    lng: -46.674069
   };
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -13,6 +13,14 @@ function initMap(){
   var marker = new google.maps.Marker({
     position: mapCenterPoint,
     map: map
+  });
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: 'testando essa paradinhaaa'
+  });
+
+  marker.addListener('click', function(){
+    infoWindow.open(map, marker);
   });
 
 };

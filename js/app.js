fd412200af6afb9c3a46a3499e7432968a2184a5
Pedro Knoll <p.giacometo@gmail.com>
2018-07-30 15:58:38 -0300

    Refactor the code

--- js/app.js
+++ js/app.js
@@ -3,24 +3,99 @@
 var map;
 
 
+// initizalizes the map
+function initMap() {
+	// Set up the map constructor, which only requires center and zoom
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -23.582944, lng: -46.674069},
+    zoom: 12
+	});
+}
+
+// show array of venues
+var venueData =  [
+    {
+      name: '90 Graus',
+      description: 'A 90 Graus foi criada no ano de 1994, tornando-se, assim, o primeiro espaço voltado exclusivamente para escalada esportiva no país.',
+      category: 'Climbing',
+      lat:  -23.634663,
+      lng: -46.659558
+    },
+    {
+      name: 'Casa de Pedra Moema',
+      description: 'A Casa de Pedra é o maior Ginásio de Escalada Esportiva do país. A unidade de Moema é a segunda da empresa e conta com dezenas de Boulderes, espaço de escalada exclusivo para crianças CLIMB4FUN®, além de sala de Yoga, equipamentos funcionais e muito mais..',
+      category: 'Climbing',
+      lat: -23.608335,
+      lng: -46.654981
+    },
+    {
+      name: 'Casa de Pedra Perdizes',
+      description: 'A Casa de Pedra é o maior Ginásio de Escalada Esportiva do país. A unidade de Perdizes é a primeira da empresa e conta com com mais de 30 TOP ROPES, Boulderes e Escalada Guiada com paredes de até 20m de altura!',
+      category: 'Climbing',
+      lat: -23.530407 ,
+      lng: -46.678495
+    },
+    {
+      name: 'Fábrica Escalada',
+      description: 'Na Fabrica o foco é a escalada em Boulder. Nada de cordas, cadeirinhas ou mosquetões. São paredes de até 4 metros e meio altura com muitos problemas de todos os níveis que exigirão de sua força e raciocínio para serem superados.',
+      category: 'Climbing',
+      lat: -23.582944,
+      lng: -46.674069
+    },
+    {
+      name: 'Tracer Parkour Faria Lima',
+      description: 'Primeira unidade da Tracer Parkour, foi o primeiro espaço  inteiramente dedicado para a prática de Parkour no Brasil.',
+      category: 'Parkour',
+      lat: -23.563694,
+      lng: -46.692074
+    },
+    {
+      name: 'Tracer Parkour Cerro Corá',
+      description: 'Segunda unidade da Tracer Parkour, o espaço possui uma variedade de obstáculos móveis e imóveis, proporcionando uma infinita combinação de possibilidade de treinos de técnicas de Parkour.',
+      category: 'Parkour',
+      lat: -23.54124,
+      lng: -46.700801
+    }
+  ]; // venueData
+
+
+
+
+
+
+
+
+
+markers = [];
+
+var addMarkers = function(){
+  for()
+}
+
+
+
+
+
+
 
 
 var ViewModel = function(){
   var self = this;
 
-  this.map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -23.582944, lng: -46.674069},
-    zoom: 12
-  });
-
-  // menu toggle visibilty
+  self.venueList = ko.observableArray([]);
   self.isVisible = ko.observable(false);
+
+
+  //
+  venueData.forEach(function(venueItem){
+    self.venueList.push(new Venue(venueItem));
+  })
+
+  // show/hide sidebar when the toggle button is clicked
   self.toggleVisibility = function(){
     self.isVisible(!self.isVisible());
   }
 
 }
 
-function initApp(){
-  ko.applyBindings(new ViewModel());
-}
+ko.applyBindings(new ViewModel());
